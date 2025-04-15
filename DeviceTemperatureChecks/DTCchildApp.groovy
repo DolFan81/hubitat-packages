6400e0f82bb041040f8d916c109f34abf82ca711
Scott <19326296+DolFan81@users.noreply.github.com>
2025-04-15 16:04:12 -0400

    Update DTCchildApp.groovy

--- DeviceTemperatureChecks/DTCchildApp.groovy
+++ DeviceTemperatureChecks/DTCchildApp.groovy
@@ -55,13 +55,13 @@ preferences {
        	if(sendPushMessage)
        	{
         	input "optionQuietTime", "bool", title: "Set a quiet time for notifications?", defaultValue: false, required: false, submitOnChange: true
+	    	if(optionQuietTime)
+   			{
+   				input (name: "startQuietTime", type: "time", title: "Select Start Time?", defaultValue: false, required: true, width:3)
+   				input (name: "endQuietTime", type: "time", title: "Select End Time?", defaultValue: false, required: true, width:3)
+   			}
+   			paragraph "<hr>"
         }
-	    if(optionQuietTime)
-   		{
-   			input "startQuietTime", "Time", title: "Select Start Time?", defaultValue: false, required: true, width:3
-   			input "endQuietTime", "Time", title: "Select End Time?", defaultValue: false, required: true, width:3
-   		}
-   		paragraph "<hr>"       	
         input "createLogEntry", "bool", title: "Create a log entry when check fails?", defaultValue: false, required: false, width:3, submitOnChange: true
        	if(createLogEntry)
    	   	{
